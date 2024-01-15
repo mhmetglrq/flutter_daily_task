2714ef187627df1ec9d8ddc97e8d494b3fb53d5a
Mehmet Güler <mhmetglr.q@gmail.com>
2024-01-15 22:59:09 +0300

    Theme.dart created!

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_daily_task/config/items/colors.dart';
 import 'package:flutter_daily_task/config/routes/app_route_names.dart';
+import 'package:flutter_daily_task/config/theme/app_theme.dart';
 
 import 'config/routes/app_routes.dart';
 import 'features/home/presentation/bloc/home_bloc.dart';
@@ -26,23 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Daily Task',
-      theme: ThemeData(
-        scaffoldBackgroundColor: AppColors.scaffoldColor,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.scaffoldColor,
-          elevation: 0,
-          iconTheme: IconThemeData(color: AppColors.titleTextColor),
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: AppColors.scaffoldColor,
-          elevation: 0,
-          selectedItemColor: AppColors.activeColor,
-          unselectedItemColor: AppColors.inactiveColor,
-          type: BottomNavigationBarType.shifting,
-        ),
-      ),
+      theme: AppTheme.lightTheme,
       onGenerateRoute: AppRoutes.onGenerateRoutes,
       initialRoute: AppRouteNames.signUp,
     );
