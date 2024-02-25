e9ae3e3687032abbc66f695d58d4f1837e4e0144
Mehmet Güler <mhmetglr.q@gmail.com>
2024-02-25 22:59:33 +0300

    Add getProjectStatus method to FirebaseProjectService and ProjectRepositoryImpl

--- lib/features/dailyTask/data/data_sources/cloud/firebase_project_service.dart
+++ lib/features/dailyTask/data/data_sources/cloud/firebase_project_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_daily_task/features/dailyTask/data/model/project.dart';
+import 'package:flutter_daily_task/features/dailyTask/data/model/status.dart';
 
 class FirebaseProjectService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -13,5 +14,18 @@ class FirebaseProjectService {
         .collection("projects")
         .doc(project.uid)
         .set(project.toMap());
+  }
+
+  Future<StatusModel?> getProjectStatus(String projectId) async {
+    return await _firestore
+        .collection("users")
+        .doc(_auth.currentUser!.uid)
+        .collection("projects")
+        .doc(projectId)
+        .get()
+        .then((value) {
+      final projectModel = ProjectModel.fromMap(value.data()!);
+      return StatusModel.fromEntity(projectModel.status!);
+    });
   }
 }
--- lib/features/dailyTask/domain/repository/project_repository.dart
+++ lib/features/dailyTask/domain/repository/project_repository.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_daily_task/core/resources/data_state.dart';
+import 'package:flutter_daily_task/features/dailyTask/data/model/status.dart';
 import 'package:flutter_daily_task/features/dailyTask/domain/entities/project.dart';
 
 abstract class ProjectRepository {
   Future<DataState<void>> createProject(ProjectEntity project);
+  Future<DataState<StatusModel?>> getProjectStatus(String projectId);
 }
