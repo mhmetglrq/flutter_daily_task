460b3ada2746f1efba0f6eb19efb00488811a47a
mhmetglrq <mhmetglr.q@gmail.com>
2024-08-22 17:43:51 +0300

    Refactor AuthRepositoryImpl to return UserModel instead of UserEntity

--- lib/features/dailyTask/domain/repository/auth_repository.dart
+++ lib/features/dailyTask/domain/repository/auth_repository.dart
@@ -2,10 +2,11 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_daily_task/features/dailyTask/domain/entities/user.dart';
 
 import '../../../../core/resources/data_state.dart';
+import '../../data/model/user.dart';
 
 abstract class AuthRepository {
   Future<DataState<User>> signIn(String username, String password);
   Future<DataState<void>> signOut();
-  Future<DataState<User>> createUser(UserEntity user);
+  Future<DataState<UserEntity>> createUser(UserEntity user);
   Future<DataState<UserEntity>> getUser();
 }
