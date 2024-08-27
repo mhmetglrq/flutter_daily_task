5f2fe09920e5e292577dc5cea8b76a2b24f66b08
mhmetglrq <mhmetglr.q@gmail.com>
2024-08-27 16:28:16 +0300

    Refactor auth repository to return UserModel instead of UserEntity

--- lib/features/dailyTask/domain/repository/auth_repository.dart
+++ lib/features/dailyTask/domain/repository/auth_repository.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_daily_task/features/dailyTask/domain/entities/user.dart';
 
 import '../../../../core/resources/data_state.dart';
-import '../../data/model/user.dart';
 
 abstract class AuthRepository {
   Future<DataState<User>> signIn(String username, String password);
--- lib/features/dailyTask/domain/usecases/auth/sign_up_usecase.dart
+++ lib/features/dailyTask/domain/usecases/auth/sign_up_usecase.dart
@@ -4,13 +4,20 @@ import 'package:flutter_daily_task/features/dailyTask/domain/repository/auth_rep
 import '../../../../../../core/resources/data_state.dart';
 import '../../../../../../core/usecase/usecase.dart';
 
-class SignUpUseCase implements UseCase<DataState<void>, UserEntity> {
+class SignUpUseCase
+    implements UseCase<DataState<UserEntity>, SignUpCaseParams> {
   final AuthRepository _authRepository;
 
   SignUpUseCase(this._authRepository);
 
   @override
-  Future<DataState<void>> call({UserEntity? params}) async {
-    return await _authRepository.createUser(params!);
+  Future<DataState<UserEntity>> call({SignUpCaseParams? params}) async {
+    return await _authRepository.createUser(params!.user);
   }
+}
+
+class SignUpCaseParams {
+  final UserEntity user;
+
+  SignUpCaseParams(this.user);
 }
