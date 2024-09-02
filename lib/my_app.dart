c646bc5a377b9664bc7d0178fa77ca82be31b25a
Mehmet Guler <mhmetglr.q@gmail.com>
2024-09-02 23:38:47 +0300

    Add initial route to RemoteAuthDone state

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_daily_task/config/routes/route_names.dart';
 import 'package:flutter_daily_task/config/routes/app_router.dart';
 import 'package:flutter_daily_task/config/theme/app_theme.dart';
 
 import 'features/dailyTask/presentation/bloc/auth/remote/remote_auth_bloc.dart';
 import 'features/dailyTask/presentation/bloc/auth/remote/remote_auth_state.dart';
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<RemoteAuthBloc, RemoteAuthState>(
+    bool isLoading = true;
+    return BlocConsumer<RemoteAuthBloc, RemoteAuthState>(
       builder: (buildContext, state) {
-        return MaterialApp(
-          title: 'Daily Task',
-          theme: AppTheme.darkTheme(buildContext),
-          debugShowCheckedModeBanner: false,
-          onGenerateRoute: AppRouter.generateRoute,
-          initialRoute:
-              state.user != null ? RouteNames.home : RouteNames.signIn,
-        );
+        return isLoading
+            ? const Material(
+                child: Center(
+                  child: CircularProgressIndicator.adaptive(
+                    backgroundColor: Colors.amber,
+                  ),
+                ),
+              )
+            : MaterialApp(
+                title: 'Daily Task',
+                theme: AppTheme.darkTheme(buildContext),
+                debugShowCheckedModeBanner: false,
+                onGenerateRoute: AppRouter.generateRoute,
+                initialRoute: state.initialRoute,
+              );
+      },
+      listener: (BuildContext context, RemoteAuthState state) {
+        if (state is RemoteAuthDone) {
+          isLoading = false;
+        }
       },
     );
   }
